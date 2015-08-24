da45dfd03a789b32d00092d00503336d4daee27f
Allister Antosik <me@allisterantosik.com>
2015-08-24 13:26:12 +0100

    Fixed bug in side nav mobile view for logged in users

--- app/views/mileages/yearly.js
+++ app/views/mileages/yearly.js
@@ -2,8 +2,6 @@ import Ember from "ember";
 
 export default Ember.View.extend({
   didInsertElement: function(){
-    Ember.$('.tooltipped').tooltip({delay: 50});
-    Ember.$('.parallax').parallax();
     Ember.$(".button-collapse").sideNav({closeOnClick: true });
   }
 });
